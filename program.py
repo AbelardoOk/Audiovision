23f977d348efdb9cec08add49f1b343f0ab6b126
Abelardo Palácios <71283403+AbelardoOk@users.noreply.github.com>
2024-08-29 01:50:46 +0100

    Refactor: Mudanças no método de captura de imagem

--- program.py
+++ program.py
@@ -1,10 +1,14 @@
 from ultralytics import YOLO
 import cv2
-from translate import Translator as tl
 import pyttsx3
 import RPi.GPIO as GPIO
 import time
 import os
+import numpy as np
+#import translate as tl
+
+#from_code = "en"
+#to_code = "pt"
 
 # Configuração botão
 botao_pin = 17
@@ -15,30 +19,53 @@ GPIO.setup(botao_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 model = YOLO('yolov8n.pt')
 
 # Configurações de fala
-translator = tl(to_lang="pt-BR")
+#translator = tl(to_lang="pt-BR")
 
 engine = pyttsx3.init()
 engine.setProperty('voice', 'brazil')
 rate = engine.getProperty('rate')
 engine.setProperty('rate', rate-40)
 
-# Função fala
 def falar(texto):
     engine.say(f"Estou vento {texto}")
     engine.runAndWait()
     engine.stop()
 
+
 # Configuração foto
-comandoFoto = "ffmpeg -f video4linux2 -s 640x480 -ss 4 -i /dev/video0 -frames 1 ./foto.jpg"
+cap = cv2.VideoCapture(0)
+
+def ajustar_balanco_branco(imagem):
+    resultado = cv2.cvtColor(imagem, cv2.COLOR_BGR2LAB)
+    avg_a = np.average(resultado[:, :, 1])
+    avg_b = np.average(resultado[:, :, 2])
+    resultado[:, :, 1] = resultado[:, :, 1] - ((avg_a - 128) * (resultado[:, :, 0] / 255.0) * 1.1)
+    resultado[:, :, 2] = resultado[:, :, 2] - ((avg_b - 128) * (resultado[:, :, 0] / 255.0) * 1.1)
+    return cv2.cvtColor(resultado, cv2.COLOR_LAB2BGR)
+    
+def iniciar_camera():
+    cap = cv2.VideoCapture(0)  # Tente abrir a câmera (0 para a webcam padrão)
+    if not cap.isOpened():
+        print("Erro ao acessar a câmera.")
+    return cap
+
+def tirarFoto(cap):
+    time.sleep(1)
+    for i in range(30):
+       ret, frame = cap.read()
+       frame = ajustar_balanco_branco(frame)
+    return frame
+
+print("Pressione o botão para começar a detectar")
 
 # Processamento foto
 try:
   while True:
-      if GPIO.input(botao_pin) == GPIO.LOW:
-          os.system("rm foto.jpg")
-          os.system(comandoFoto)
+      if not cap.isOpened():
+        cap = iniciar_camera()
         
-          img = cv2.imread("foto.jpg")
+      if GPIO.input(botao_pin) == GPIO.LOW:
+          img = tirarFoto(cap)
           resultado = model(img, stream=True)
           
           img = cv2.flip(img,-1)
@@ -53,14 +80,17 @@ try:
               for box in boxes:
                   # Classe detectada
                   cls = int(box.cls[0])
-                  objeto = translator.translate(classNames[cls])
+                  objeto = classNames[cls]
+                  #objeto = translator.translate(classNames[cls])
                   objetos.append(objeto)
 
-          print(f"Objetos detectados: {objetos    }")
+          print(f"Objetos detectados: {objetos}")
           falar(objetos)
+          print("\nPressione o botão para começar a detectar")
+          cap.release()
 
-      else:
-          print("Botão não prssionado")
+      #else:
+          #print("Botão não prssionado")
 
       if cv2.waitKey(1) & 0xFF == ord("q"):
         break
